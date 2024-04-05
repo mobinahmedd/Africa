cccee0422c5f15f1896cd1100e0cc716d7c81c79
Mobin Ahmed <mobinahmed4321@gmail.com>
2024-04-05 00:06:54 -0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Apptycoons on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
